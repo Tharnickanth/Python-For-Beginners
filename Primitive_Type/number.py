8592df7dea1208b5454210fa7dad7221b25d5b05
Tharnickanth Manoranjan <tharnimo@gmail.com>
2021-05-22 14:53:34 +0530

    Numbers

---
+++ Primitive_Type/number.py
@@ -0,0 +1,17 @@
+
+x = 12
+y = 4.43
+
+print(10/3)
+print(10//3)
+print(10**3)
+print(round(11/3))
+print(abs(-437))
+
+
+a = input("Enter your age : ")
+print(f"Your age : {a}")
+
+p = input("P : ")
+q = int(p)+1
+print(q)
