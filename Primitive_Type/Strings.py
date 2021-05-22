aa7565d06679fe7eafd5eef34204f51c0dc39119
Tharnickanth Manoranjan <tharnimo@gmail.com>
2021-05-22 13:38:16 +0530

    adding some contains

--- Primitive_Type/Strings.py
+++ Primitive_Type/Strings.py
@@ -6,3 +6,20 @@ print(Name)
 print(Name[0:12])
 print(Name[13:])
 print(Name[3:16])
+
+
+Letter = """
+Hi Tharnickanth,
+
+How are you.....
+
+.......................
+...................
+.......................
+
+"""
+
+print(Letter)
+
+Course = "Learning \"Python\" for Beginners"
+print(Course)
