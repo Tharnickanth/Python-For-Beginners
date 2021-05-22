d61cdce7d9202c841f38713f154d0a0254e4a195
Tharnickanth Manoranjan <tharnimo@gmail.com>
2021-05-22 14:35:03 +0530

    Strings Methods

--- Primitive_Type/Strings.py
+++ Primitive_Type/Strings.py
@@ -25,9 +25,22 @@ Course = "Learning \nPython \nfor \nBeginners"
 print(Course)
 print(len(Course))
 
+# formatted strings
 fName = "Tharnickanth"
 lName = "Manoranjan"
 fullName = fName+" "+lName
 print(fullName)
 print(f"{fName} {lName}")
 print(f"{len(fName)} {lName}")
+
+# String Methods
+
+course = " python programming "
+print(course.upper())
+print(course.lower())
+print(course. title())
+print(course.strip())
+print(course.rstrip())
+print(course.lstrip())
+print("pro" in course)
+print("the" not in course)
