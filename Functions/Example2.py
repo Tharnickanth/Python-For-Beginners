e0c24a9e8e49b06233b53bc60835d26b4c2f769e
Tharnickanth Manoranjan <tharnimo@gmail.com>
2021-05-24 16:38:37 +0530

    a

--- Functions/Example2.py
+++ Functions/Example2.py
@@ -6,3 +6,29 @@ def getMessage(name, age):
 
 message = getMessage(age=25, name="Tharnickanth")
 print(message)
+
+
+def increment(number, by=25):
+    return number+by
+
+
+total = increment(100, 1000)
+print(total)
+
+
+def multiply(*number):
+    total = 1
+    for num in number:
+        total *= num
+
+    return total
+
+
+print(multiply(2, 4, 5, 7))
+
+
+def details(**user):
+    print(user)
+
+
+details(name="Tharnickanth", age=34, address="Kalmunai")
