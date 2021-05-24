a143a83e2ea3997d43fec7d406dcc466cc559ea8
Tharnickanth Manoranjan <tharnimo@gmail.com>
2021-05-24 14:14:59 +0530

    ReturnValue

--- Functions/DefiningFunction.py
+++ Functions/DefiningFunction.py
@@ -1,6 +1,24 @@
-
+# Defining
 def Greet():
     print("Hi Tharni")
 
 
 Greet()
+
+# Arguments
+
+
+def printName(firstName, lastName):
+    print(f"{firstName} {lastName}")
+
+
+printName("Tharnickanth", "Manoranjan")
+
+# returnValue
+
+
+def averageOfNumbers(number1, number2):
+    return (number1+number2)/2
+
+
+print(f"Average : {averageOfNumbers}")
